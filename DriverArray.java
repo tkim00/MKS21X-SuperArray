9e5bb02f7ee90a93c59d0ba2b6d4f911ab5f5c3d
Taejoon Kim <tkim00@stuy.edu>
2018-10-25 11:00:26 -0400

    phase 1 finished

--- DriverArray.java
+++ DriverArray.java
@@ -1,5 +1,19 @@
 public class DriverArray{
   public static void main(String[] args){
-    
+    SuperArray d1 = new SuperArray();
+
+    System.out.println(d1.isEmpty());
+
+    for(int i = 0; i < 10; i++){
+      d1.set(i, "" + i+1);
+    }
+    System.out.println(d1);
+
+    System.out.println(d1.get(4));
+    System.out.println(d1.get(10));
+    d1.set(10, "11");
+
+    d1.clear();
+    System.out.println(d1.size());
   }
 }
--- SuperArray.java
+++ SuperArray.java
@@ -17,36 +17,40 @@ public class SuperArray{
   }
 
   public boolean add(String element){
-    if(size == data.length){
+    /*if(size == data.length){
       data.resize();
-    }
+    }*/
     data[size] = element;
     return true;
   }
   public String toString(){
     String list = "[";
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < size(); i++){
       list += data[i] + ",";
     }
     return list + "]";
   }
   public String toStringDebug(){
     String list = "[";
-    for(int i = 0; i data.length< ; i++){
+    for(int i = 0; i < data.length ; i++){
       list += data[i] + ",";
     }
     return list + "]";
   }
   public String get(int index){
     if(index < 0 || index >= size()){
+      System.out.println("error");
       return null;
     }
     return data[index];
   }
   public String set(int index, String element){
     if(index < 0 || index >= size()){
+      System.out.println("error");
       return null;
     }
     data[index] = element;
+    size++;
+    return "";
   }
 }
